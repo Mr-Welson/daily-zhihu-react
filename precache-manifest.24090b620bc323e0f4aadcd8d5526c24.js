d6aa629693da6b0f557a72d851e4362a0f4149d1
welson <468905342@qq.com>
2019-07-30 17:45:14 +0800

    Updates

---
+++ precache-manifest.24090b620bc323e0f4aadcd8d5526c24.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "495750061d5c97a0162f09c68b76514b",
+    "url": "/daily-zhihu-react/index.html"
+  },
+  {
+    "revision": "4322247a6b66b6120bcb",
+    "url": "/daily-zhihu-react/static/css/3.30fc13f9.chunk.css"
+  },
+  {
+    "revision": "5fba4b6494e267be46f2",
+    "url": "/daily-zhihu-react/static/css/main.39eff18f.chunk.css"
+  },
+  {
+    "revision": "1c219f411da30cbf2cde",
+    "url": "/daily-zhihu-react/static/js/2.22b5cc16.chunk.js"
+  },
+  {
+    "revision": "4322247a6b66b6120bcb",
+    "url": "/daily-zhihu-react/static/js/3.b52ef1aa.chunk.js"
+  },
+  {
+    "revision": "5fba4b6494e267be46f2",
+    "url": "/daily-zhihu-react/static/js/main.a5ced450.chunk.js"
+  },
+  {
+    "revision": "22dd9715643f5b048e46",
+    "url": "/daily-zhihu-react/static/js/runtime~main.3d372b3a.js"
+  },
+  {
+    "revision": "2d902be2d89f57af16bb5063c23188b7",
+    "url": "/daily-zhihu-react/static/media/nodata.2d902be2.svg"
+  }
+]);
