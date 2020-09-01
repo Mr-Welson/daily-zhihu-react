c6949e47346fdc95bae18db61a5c9924b100447b
welson <songwei@lingdanet.com>
2020-09-01 15:09:45 +0800

    优化文章列表展示

--- src/containers/ArticleList/index.js
+++ src/containers/ArticleList/index.js
@@ -7,30 +7,28 @@ const ArticleList = ({ data }) => {
   return (
     <div className="article-list-container">
       {!!data.length &&
-        data.map(v => {
-          return (
-            <div key={v.date} className="list-content">
-              <div className="list-date">
-                {v.date.substr(0, 4) +
-                  "/" +
-                  v.date.substr(4, 2) +
-                  "/" +
-                  v.date.substr(6, 2)}
-              </div>
-              <div className="article-list">
-                {v.stories &&
-                  v.stories.map(article => (
-                    <Link to={`/article/${article.id}`} key={article.id}>
-                      <div className="article">
-                        <img src={article.images[0]} alt="" />
-                        <div className="article-info">{article.title}</div>
-                      </div>
-                    </Link>
-                  ))}
-              </div>
+        data.map(v => (
+          <div key={v.date} className="list-content">
+            <div className="list-date">
+              {v.date.substr(0, 4) +
+                "/" +
+                v.date.substr(4, 2) +
+                "/" +
+                v.date.substr(6, 2)}
             </div>
-          );
-        })}
+            <div className="article-list">
+              {v.stories &&
+                v.stories.map(article => (
+                  <Link to={`/article/${article.id}`} key={article.id}>
+                    <div className="article">
+                      <img src={article.images[0]} alt="" />
+                      <div className="article-info">{article.title}</div>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+          </div>
+        ))}
     </div>
   );
 };
